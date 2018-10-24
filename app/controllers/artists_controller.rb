40ef1e795af899f4f2b8f07dac4d41adb4e2cb39
geoffrey woodcock <theatomproject@gmail.com>
2018-10-24 19:21:37 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,17 +8,11 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+    
   end
 
   def create
-    @artist = Artist.new(artist_params)
-
-    if @artist.save
-      redirect_to @artist
-    else
-      render :new
-    end
+    
   end
 
   def edit
